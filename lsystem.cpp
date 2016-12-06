e8c7262d82a3abf9d5f87dfaf0d33569f8ff67b7
Liam Ozog <lozog@uwaterloo.ca>
2016-12-05 21:07:06 -0500

    removed unneeded function

--- lsystem.cpp
+++ lsystem.cpp
@@ -3,18 +3,6 @@
 #include <iostream>
 using namespace std;
 
-// helper function
-// modifies subject so all occurrences of search are changed to replace
-// taken from http://stackoverflow.com/questions/5878775/how-to-find-and-replace-string
-void ReplaceString(string& subject, const string& search,
-                          const string& replace) {
-    size_t pos = 0;
-    while ((pos = subject.find(search, pos)) != string::npos) {
-        subject.replace(pos, search.length(), replace);
-        pos += replace.length();
-    } // while
-}
-
 string LSystem::generateExpr( string axiom,
 									 Rules rules,
 									 unsigned int depth ) {
